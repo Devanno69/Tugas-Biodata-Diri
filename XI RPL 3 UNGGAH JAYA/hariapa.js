334761ac068488b54a8c6dd6f070734c1e155b51
Devanno69 <devannosatriaananta@gmail.com>
2024-08-21 14:56:33 +0800

    Add files via upload

---
+++ XI RPL 3 UNGGAH JAYA/hariapa.js
@@ -0,0 +1,33 @@
+function toggleVisibility0() {
+    var tabel0 = document.getElementById("tabel0");
+
+    //Tabel 0
+    if (tabel0.style.display === "none") {
+        tabel0.style.display = "block";
+    } else {
+        tabel0.style.display = "none";
+    }
+}
+
+function toggleVisibility1() {
+    var tabel0 = document.getElementById("tabel0");
+
+    //Tabel 1
+    if (tabel1.style.display === "none") {
+        tabel1.style.display = "block";
+        Button.textContent = "Show Table"
+    } else {
+        tabel1.style.display = "none";
+    }
+}
+
+function toggleVisibility2() {
+    var tabel0 = document.getElementById("tabel0");
+
+    //Tabel 2
+    if (tabel2.style.display === "none") {
+        tabel2.style.display = "block";
+    } else {
+        tabel2.style.display = "none";
+    }
+}
